108d811a13984a12a639ca0e1c1d955f74ece05b
Ricardo Blas <ricardo.blas@gmail.com>
2016-07-26 01:52:10 +0000

    actualizando scaffold Ubigeo y SpecialFeature, usando Seeds para precargar la data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :special_features
   resources :ubigeos
+  resources :ubigeos
+  resources :special_features
+  resources :ubigeos
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :profiles, only: [:edit, :update]
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20160725215006) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "user_interactions", force: :cascade do |t|
+    t.text     "question",      limit: 65535
+    t.text     "answer",        limit: 65535
+    t.integer  "parkinglot_id", limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "user_interactions", ["parkinglot_id"], name: "index_user_interactions_on_parkinglot_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
