91a5c8b8b1eef002ee090fd87278131ceddf6efa
Ricardo Blas <ricardo.blas@gmail.com>
2016-07-30 00:53:18 +0000

    changing menu to allow promote parking spaces only to authenticated users

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,16 +1,16 @@
 class BaseController < ApplicationController
     # restrict access to authenticated users
   before_action :authenticate_user!
-  before_filter :admin_only, :except => :tos
+#   before_filter :admin_only, :except => :tos
   
-  def admin_only
-    if current_user.role != "admin" then
-        redirect_to :back, :alert => "Access denied."
-    end
-    # unless current_user.admin?
-    #   redirect_to :back, :alert => "Access denied."
-    # end
-  end
+#   def admin_only
+#     if current_user.role == "user" then
+#         redirect_to :back, :alert => "Access denied."
+#     end
+#     # unless current_user.admin?
+#     #   redirect_to :back, :alert => "Access denied."
+#     # end
+#   end
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
